fb0ea3eb4bb56968a230757d23fc7eacc7f47aed
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-23 00:47:03 +0200

    Create Ex-01.js

---
+++ Module-02/Ex-01/Ex-01.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const num1 = parseInt(prompt(`Enter the first number: `));
+const num2 = parseInt(prompt(`Enter the second number: `));
+const num3 = parseInt(prompt(`Enter the third number: `));
+const num4 = parseInt(prompt(`Enter the forth number: `));
+const num5 = parseInt(prompt(`Enter the fifth number: `));
+
+let numberList = [];
+numbernumist.push(num1);
+numberist.push(num2);
+numberList.push(num3);
+numberList.push(num4);
+numberList.push(num5);
+
+for (let index = numberList.length - 1; index >= 0; index--) {
+    const element = numberList[index];
+    console.log(element);
+  }
