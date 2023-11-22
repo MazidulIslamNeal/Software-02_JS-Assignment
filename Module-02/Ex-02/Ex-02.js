9e1311c3f341ab09b21806068a3d21d3502dce74
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-23 00:48:10 +0200

    Create Ex-02.js

---
+++ Module-02/Ex-02/Ex-02.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const people = parseInt(prompt(`How many people are attending?`));
+let parts = [];
+ 
+for (let i = 0; i < people; i++) {
+    let name = prompt("Enter the names of participants:");
+    parts.push(name);
+}
+parts.sort();
+
+var el = document.getElementById('list');
+var ol = document.createElement('ol');
+el.appendChild(ol);
+parts.forEach(function (part) {
+  var f = document.createElement('li');
+  f.appendChild(document.createTextNode(part));
+  ol.appendChild(f);
+});
