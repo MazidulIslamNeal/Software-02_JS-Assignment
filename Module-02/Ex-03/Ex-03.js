d4d4cdcc2a94283ccdada93cb18c42e65305cae3
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-23 00:49:07 +0200

    Create Ex-03.js

---
+++ Module-02/Ex-03/Ex-03.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let dogs = [];
+
+for (let i = 0; i < 7; i++) {
+    let name = prompt(`Enter the dog's name?`);
+    dogs.push(name);
+}
+
+dogs.sort();
+dogs.reverse();
+
+var el = document.getElementById('dog');
+var ol = document.createElement('ul');
+el.appendChild(ol);
+dogs.forEach(function (dog) {
+  var f = document.createElement('li');
+  f.appendChild(document.createTextNode(dog));
+  ol.appendChild(f);
+});
