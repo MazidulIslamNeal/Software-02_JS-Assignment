7bbc9fafdeee39ae500515f36d3e9b744f04e798
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-28 23:17:11 +0200

    Update Ex-04.js

--- Module-01/Ex-04/Ex-04.js
+++ Module-01/Ex-04/Ex-04.js
@@ -5,22 +5,6 @@ let name = prompt(`What is your name?`);
 let place = (Math.floor(Math.random() * 4) + 1); 
 console.log(place);
 
-//if (place == 1) {
-  //  document.querySelector('.place').innerHTML = (`${name}!, you are Gryffindor!`);
-//}
-
-//if (place == 2) {
-  //  document.querySelector('.place').innerHTML = (`${name}!, you are Hufflepuff!`);
-//}
-
-//if (place == 3) {
-  //  document.querySelector('.place').innerHTML = (`${name}!, you are Ravenclaw!`);
-//}
-
-//if (place == 4) {
-  //  document.querySelector('.place').innerHTML = (`${name}!, you are Slytherin!`);
-//}
-
 switch(place) {
     case 1 :
         document.querySelector('.place').innerHTML = (`${name}!, you are Gryffindor!`);
